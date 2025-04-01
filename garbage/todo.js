1d171660d6bb4e0234cecef9b772da72743af5e6
bard <wodykjakub@gmail.com>
2025-04-01 20:29:38 +0200

    first evening of coding done

---
+++ garbage/todo.js
@@ -0,0 +1,8 @@
+// ! TODO LIST !
+
+// 1. Create dummyRobot.json
+// 2. Create barely working ui for NPC generator
+// 3. create a script for NPC generator
+// 4. (when I'll be bored) create for robots and animals
+// 5. create initiative tracker
+// 6. create JSON file for each sztuczka... I WANT TO DIE
