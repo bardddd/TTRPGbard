8c28b5a273bcee057c7be5804139024c00c7c9de
bard <wodykjakub@gmail.com>
2025-04-03 21:46:03 +0200

    third night of coding done

--- garbage/todo.js
+++ garbage/todo.js
@@ -1,7 +1,7 @@
 // ! TODO LIST !
 
-// 1. Create dummyRobot.json
-// 2. Create barely working ui for NPC generator
+// 1. Create dummyRobot.json DONE
+// 2. Create barely working ui for NPC generator DONE
 // 3. create a script for NPC generator
 // 4. (when I'll be bored) create for robots and animals
 // 5. create initiative tracker
